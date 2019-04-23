948c98764546d6899fa9497b0377ad9e21f2397a
Jordan Violet <8886650+jtviolet@users.noreply.github.com>
2019-04-23 11:30:16 -0400

    corrected a few issues

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ import json
 from airtable import AirTable as at
 
 def main():
-    # get list of ongoing projects
-    project_records = get_projects()
-
-    # check each records current single-select field and current owner
-    # check who the owner should be
-    # if the 'should be' owner is different, update the record with the new owner
-    for record in project_records:
-        record_id = record['id']
-        project = record['fields']
-
-        single_select_field = project[at.SINGLE_SELECT_FIELD]
-        current_owner_id = project[at.SINGLE_SELECT_FIELD]['id']
-
-        next_owner_id = at.steps_owners[single_select_field]['id']
-
-        if current_owner_id is not next_owner_id:
-            update_project_owner(single_select_field, record_id)
+    try:
+        # get list of ongoing projects
+        project_records = get_projects()
+    
+        # check each records single-select value and assignee value
+        # check who the assignee should be
+        # if the 'should be' assignee is different, update the record with the new assignee
+        for record in project_records:
+            record_id = record['id']
+            project = record['fields']
+    
+            current_single_select_value = project[at.SINGLE_SELECT_FIELD]
+            current_assignee = project[at.ASSIGNEE_FIELD]['id']
+    
+            next_assignee = at.steps_owners[current_single_select_value]['id']
+    
+            if current_assignee is not next_assignee:
+                update_project_owner(current_single_select_value, record_id)
+    except:
+        print("There was a problem. Likely, the 'Current Stage' was not set for a record.")
 
 
 # get a list of projects from airtable
@@ -33,11 +36,11 @@ def get_projects():
     return req.json()['records']
 
 
-# update the current project owner
-def update_project_owner(single_select_field, record_id):
-    new_owner_data = {
-        'fields': {
-            at.ASSIGNEE_FIELD: at.steps_owners[single_select_field]
+# update the current assignee for the record
+def update_project_owner(current_single_select_value, record_id):
+    new_assignee = {
+        "fields": {
+            at.ASSIGNEE_FIELD: at.steps_owners[current_single_select_value]
         }
     }
 
@@ -48,10 +51,9 @@ def update_project_owner(single_select_field, record_id):
             'Authorization': f'Bearer {at.API_KEY}',
             'Content-Type': 'application/json'
         },
-        data = json.dumps(new_owner_data)
+        data = json.dumps(new_assignee)
     )
 
 
-# run the program
 if __name__ == '__main__':
     main()

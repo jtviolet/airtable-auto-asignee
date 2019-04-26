2af87b49f6143ae055d3b8026e058ffb0ca6f50f
Jordan Violet <8886650+jtviolet@users.noreply.github.com>
2019-04-26 12:55:04 -0400

    took time to fix edge cases (i.e. values not set)

--- app.py
+++ app.py
@@ -3,26 +3,31 @@ import json
 from airtable import AirTable as at
 
 def main():
-    try:
-        # get list of ongoing projects
-        project_records = get_projects()
-    
-        # check each records single-select value and assignee value
-        # check who the assignee should be
-        # if the 'should be' assignee is different, update the record with the new assignee
-        for record in project_records:
-            record_id = record['id']
-            project = record['fields']
-    
+    # get list of ongoing projects
+    project_records = get_projects()
+
+    # check each records single-select value and assignee value
+    # check who the assignee should be
+    # if the 'should be' assignee is different, update the record with the new assignee
+    for record in project_records:
+        record_id = record['id']
+        project = record['fields']
+
+        if at.SINGLE_SELECT_FIELD in project:
             current_single_select_value = project[at.SINGLE_SELECT_FIELD]
-            current_assignee = project[at.ASSIGNEE_FIELD]['id']
-    
             next_assignee = at.steps_owners[current_single_select_value]['id']
-    
-            if current_assignee is not next_assignee:
+
+            if at.ASSIGNEE_FIELD in project:
+                current_assignee = project[at.ASSIGNEE_FIELD]['id']
+                if current_assignee != next_assignee:
+                    update_project_owner(current_single_select_value, record_id)
+                else:
+                    print('No need to auto-assign assignee--correct assignee is already in place.')
+            else:
                 update_project_owner(current_single_select_value, record_id)
-    except:
-        print("There was a problem. Likely, the 'Current Stage' was not set for a record.")
+
+        else:
+            print('No need to auto-assign asignee--single-select value is not present.')
 
 
 # get a list of projects from airtable
